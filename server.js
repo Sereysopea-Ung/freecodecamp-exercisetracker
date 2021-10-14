4d6f132c2ab13bd7b53b5dd1cae59ccac930b4aa
unknown <ungsereysopeacontact@gmail.com>
2021-10-14 17:54:42 +0700

    Final Passed

--- server.js
+++ server.js
@@ -8,14 +8,14 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI);
 
 const User = mongoose.model('User', new mongoose.Schema({
-  username: {type: String, required: true},
-  log: Array
+  username: {type: String, required: true}
 }));
 
 const Exercises = mongoose.model('Exercises', new mongoose.Schema({
   description: {type:String, required: true},
   duration: {type:Number, required: true},
-  date: String
+  date: String,
+  userID: String
 }));
 
 app.use(cors())
@@ -27,8 +27,7 @@ app.get('/', (req, res) => {
 
 app.post('/api/users', (req,res)=>{
   let newUser = new User({
-    username: req.body.username,
-    log: []
+    username: req.body.username
   });
   newUser.save((err,data)=>{
     if (err) return console.log(err);
@@ -60,33 +59,55 @@ function convertDate(date){
 }
 
 app.post('/api/users/:_id/exercises',(req,res)=>{
-  User.findById(req.body[':_id'],(err,result)=>{
+  User.findById(req.params['_id'],function(err,user){
     if (err) return console.log(err);
-    if (result==null){
-      res.send("_id Not Found");
-    } else {
+    if(req.params['_id']){
       new Exercises({
         description: req.body.description,
         duration: parseInt(req.body.duration),
-        date: convertDate(req.body.date)
-      }).save((err,data)=>{
+        date: convertDate(req.body.date),
+        userID: req.params['_id']
+      }).save((err,exercise)=>{
         if (err) return console.log(err);
-        result.log.push(data);
-        result.save((err,savedresult)=>{
-          if (err) return console.log(err);
-        });
         res.json({
-          username: result.username,
-          _id: result.id,
-          date: data.date,
-          duration: data.duration,
-          description: data.description
+          username: user.username,
+          description: exercise.description,
+          duration: exercise.duration,
+          date: exercise.date,
+          _id: req.params['_id']
         });
-      }); 
+      });
+    } else {
+      return res.send("ID NOT FOUND");
     }
   });
 });
 
+app.get('/api/users/:_id/logs',(req,res)=>{
+  Exercises.find({userID: req.params._id},(err, exercisesArray)=>{
+    if (err) return console.log(err);
+    let log = exercisesArray.map((exercise)=>{ return {description:exercise.description, duration:exercise.duration,date:exercise.date}});
+    let count = log.length;
+    if (req.query.from){
+      log = log.filter((exercise)=> new Date(exercise.date).getTime() >= new Date(req.query.from).getTime());
+    }
+    if (req.query.to){
+      log = log.filter((exercise)=> new Date(exercise.date).getTime() <= new Date(req.query.to).getTime());
+    }
+    if (req.query.limit){
+      log = log.slice(0,req.query.limit);
+    }
+    User.findById(req.params._id,(err,user)=>{
+      if (err) return console.log(err);
+      res.json({
+        username: user.username,
+        count: count,
+        _id: req.params._id,
+        log: log
+      });
+    });
+  });
+});
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
